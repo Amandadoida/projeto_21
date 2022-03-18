43c6f392f687902c4140293fc5739b771cadbc7d
Amandadoida <94861251+Amandadoida@users.noreply.github.com>
2022-03-18 14:33:45 -0300

    18/03/2022

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+var chaozinho;
+var paredinha;
+var paredinhagemea;
+
+function preload()
+{
+	
+}
+
+function setup() {
+
+var ball_options={
+	isStatic:false,
+	restitution: 0.3,
+	friction:0,
+	density:1.2
+}
+
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+ball=Bodies.circle(260,100,20,ball_options);
+World.add(world,ball);
+
+	Engine.run(engine);
+  chaozinho= new Ground (width/2, 670, width, 20);
+  paredinha= new Ground (1100, 600, 20, 120);
+  paredinhagemea= new Ground (1350, 600, 20, 120);
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  
+  chaozinho.show();
+  paredinha.show();
+  paredinhagemea.show();
+ellipse(ball.position.x, ball.position.y, 40,40);
+
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+	}
+}
+
